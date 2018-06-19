92326757ce4308660ce40249ca4cf75a1a9317c6
Selene Marivaldi <bobdablob42@gmail.com>
2018-06-19 14:46:53 -0400

    style: Change UI

--- app/src/main/java/com/selsapps/selsdiceroller/MainActivity.java
+++ app/src/main/java/com/selsapps/selsdiceroller/MainActivity.java
@@ -1,9 +1,11 @@
 package com.selsapps.selsdiceroller;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,15 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView rollDice = findViewById(R.id.pickDice);
+        TextView rollStats = findViewById(R.id.pickStats);
+
+        rollDice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent diceIntent = new Intent(MainActivity.this, RollActivity.class);
+                startActivity(diceIntent);
+            }
+        });
+
+        rollStats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent statsIntent = new Intent(MainActivity.this, StatsActivity.class);
+                startActivity(statsIntent);
+            }
+        });
     }
 
-    public void goToDice (View view) {
-        Intent toDiceIntent = new Intent(this, RollActivity.class);
-        startActivity(toDiceIntent);
-    }
-
-    public void goToStats (View view) {
-        Intent toStatsIntent = new Intent(this, StatsActivity.class);
-        startActivity(toStatsIntent);
-    }
 }
